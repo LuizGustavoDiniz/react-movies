376a8d093f0903335c406739e7246f8d534c4834
luizgustavodiniz <gustavo.diniz.gon@gmail.com>
2022-08-06 13:44:13 -0300

    continuação do header e do main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import RoutesApp from "./routes";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return <RoutesApp/>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,18 @@
 import'./home.css'
-import {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
 import { Link } from 'react-router-dom'
 import api from '../../services/api'
 
 const Home = () => {
 
     const [movies, setMovies] = useState([])
+    
+    
+    
+
+    
+
 
     useEffect(() => {
 
@@ -27,6 +34,30 @@ const Home = () => {
 
     }, [])
 
+    const showMenu = (event) => {
+        const element = event.target
+        const navBar = document.querySelector('.navbar')
+
+        navBar.classList.toggle('active')
+        element.classList.toggle('fa-times')
+    
+    }
+
+    window.addEventListener('resize', () => {
+        const element = document.querySelector('#menu-burguer')
+        const navBar = document.querySelector('.navbar')
+        const width = document.body.clientWidth
+        
+
+        
+
+        if(width > 580){
+            navBar.classList.remove('active')
+       
+            element.classList.remove('fa-times')
+        }
+    })
+
   
     return(
         <>
@@ -36,11 +67,23 @@ const Home = () => {
             <div className='logo'>
               <Link  to='/'>react<span>movies</span></Link>
             </div>
-            <nav className='navbar' >
-              <Link className='menu-link' to=''>Em cartaz</Link>
-              <Link className='menu-link' to=''>Filmes</Link>
-              <Link className='menu-link' to=''>Séries</Link>
+            <nav className='navbar'>
+                 <Link className='menu-link' to=''>Em cartaz</Link>
+                 <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+                 <Link className='menu-link' to=''>Séries</Link>
             </nav>
+
+            <span onClick={(event) => showMenu(event)} className='fas fa-bars' id='menu-burguer'></span>
             </section>
         </header>
 
@@ -57,8 +100,10 @@ const Home = () => {
                         </div>
                           <div className="card-body">
                             <h5 className="card-title">{movie.title}</h5>
-                            <p className="card-text"><i className="fab fa-imdb text-light"></i></p>
-                            <a href="..." className="btn btn-primary">ver detalhes</a> 
+                            <p className="card-text"><i className="fab fa-imdb text-light"></i>Nota: 6.5/10</p>
+                            <div className='btn-container'>
+                            <a href="..." className="btn">ver detalhes</a> 
+                            </div>
                           </div>
                       </div>
                  )
